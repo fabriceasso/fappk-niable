ddfc12c0436832215c368ee4e43e6f8b54a7e4ed
fabriceasso <fabriceasso@gmail.com>
2026-03-20 14:39:54 +0000

    Configure GitHub Pages deployment via Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/fappk-niable',
   reactStrictMode: true,
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   },
