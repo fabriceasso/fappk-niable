245a681bc00e1bfe2f6c1a9d24334b3a332b836e
fabriceasso <fabriceasso@gmail.com>
2026-04-28 23:36:04 +0000

    fix: fully rely on NEXT_PUBLIC_BASE_PATH for prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production';
-const isVercel = process.env.VERCEL === '1';
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || (isProd && !isVercel ? '/fappk-niable' : '');
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
--- src/utils/prefix.js
+++ src/utils/prefix.js
@@ -1,5 +1,3 @@
-const isProd = process.env.NODE_ENV === 'production';
-const isVercel = process.env.NEXT_PUBLIC_VERCEL === '1';
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH || (isProd && !isVercel ? '/fappk-niable' : '');
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 export default prefix;
